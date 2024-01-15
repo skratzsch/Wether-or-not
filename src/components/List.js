730a3c9fa3535794cb54d755058b233749a668b7
skratzsch <skratzsch@yahoo.de>
2024-01-15 11:27:59 +0100

    add styling for list

--- src/components/List.js
+++ src/components/List.js
@@ -3,11 +3,31 @@ import styled from 'styled-components';
 
 const StyledList = styled.div`
   margin-bottom: 10px;
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+    background-color: rgba(255, 255, 255, 0.3);
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    padding: 5px;
+  }
+
+  li button {
+    margin-left: 10px;
+  }
 `;
 
 function List({ activities, isGoodWeather, onDeleteActivity }) {
   return (
-    <div>
+    <StyledList>
       <h2>{isGoodWeather ? 'Das Wetter ist super, geh raus und tob dich aus!' : 'Aktivitäten für schlechtes Wetter'}</h2>
       <ul>
         {activities.length > 0 ? (
@@ -21,7 +41,7 @@ function List({ activities, isGoodWeather, onDeleteActivity }) {
           <p>Keine Aktivitäten vorhanden.</p>
         )}
       </ul>
-    </div>
+    </StyledList>
   );
 }
 
